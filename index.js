0e25516bffeb2269f0523953e82ed14f0243939e
Mahmood Nihad <m.zibari@gmail.com>
2020-02-06 19:47:28 -0600

    add comments to code

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 $(function () {
+  //This event listner will listen to when the add item button in pressed 
   $("#js-shopping-list-form").submit(event => {
     event.preventDefault();
+
+    //Retrieving the new item from the input element and storing it in newItem varible
     const newItem = $(this).find('input[name=shopping-list-entry]').val();
+
+    //Adding the new list item to the <ul> using the .append() method
     $('.shopping-list').append(`<li>\
             <span class="shopping-item">${newItem}</span>\
             <div class="shopping-item-controls">\
@@ -16,12 +21,17 @@ $(function () {
   });//end of the shopping list submit function
   
 
+  //Checking off items using event delegation
   $('ul').on('click', 'button[class="shopping-item-toggle"]', function (event) {
+    //Using .closest() method will only select direct ancestors, so I moved up the tree to <li> ancestor,
+    //then moved down to its child <span> using .find() method, then toggeled the class using .toggleClass()
     $(this).closest('li').find('span.shopping-item').toggleClass(' shopping-item__checked');
 
   });//end of check function
 
+  //Deleting items again using event delegation
   $('ul').on('click', 'button[class="shopping-item-delete"]', function (event) {
+    //Since <li> is a direct ancestor, I only needed to use .closest() method then .remove() method to delete item
     $(this).closest('li').remove();
 
   });//end of delete function
